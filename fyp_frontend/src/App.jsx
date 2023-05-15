75fdcc93bc6316aced7a087037ba31280617418b
Mayank Shetty <mayanks928@gmail.com>
2023-05-15 23:46:36 +0530

    Extended home page, and stylinh

--- fyp_frontend/src/App.jsx
+++ fyp_frontend/src/App.jsx
@@ -8,28 +8,101 @@ import Col from "react-bootstrap/Col";
 import Myimageslider from "./components/Myimageslider/Myimageslider";
 
 function App() {
-
   return (
     <div>
       <NavBar />
       <Container lg>
-        <Row>
-          <Col md={12} lg={6} className="homeImage1" >
-          <Myimageslider width={"500px"} height={"500px"}/>
+        <Row className="py-4">
+          <Col md={12} lg={6} className="homeImage1">
+            <Myimageslider width={"500px"} height={"500px"} />
           </Col>
-          <Col md={12} lg={6} >
-            <div className="content">
-              <div className="textCard">
-                <h2 className="display-1">AI Image Enhancement Tools</h2>
-                <p>
-                  Tired of blurry, low-resolution, or damaged images? Our
-                  AI-powered image processing techniques can help you achieve
-                  clear and high-quality images.
-                </p>
-              </div>
+          <Col
+            md={12}
+            lg={6}
+            className="d-flex flex-column justify-content-center align-items-center"
+          >
+            <div className="textTitleDiv">
+              <h2>AI Image Enhancement Tools</h2>
+              <p>
+                Tired of blurry, low-resolution, or damaged images? Our
+                AI-powered image processing techniques can help you achieve
+                clear and high-quality images.
+              </p>
             </div>
           </Col>
         </Row>
+        <hr className="whiteHr" />
+        <Row className="py-4">
+          <Col md={12} lg={12}>
+            <div className="textTitleDiv">
+              <h2 className="textTitle">
+                Automatically enhance your images for personal and business
+                needs
+              </h2>
+            </div>
+          </Col>
+        </Row>
+        <hr className="whiteHr" />
+        <Row className="py-4">
+          <Col
+            md={12}
+            lg={8}
+            className="d-flex flex-column justify-content-center align-items-center"
+          >
+            <div className="textTitleDiv2">
+              <h3>Unblur image with advanced AI</h3>
+              <p>
+                Retouch and fix the blurry image using our advanced AI
+                enhancement technology. Its time to unblur image for a clearer,
+                crisper, and more precise result.
+              </p>
+            </div>
+          </Col>
+          <Col md={12} lg={4} className="homeImage1">
+            <Myimageslider width={"500px"} height={"400px"} />
+          </Col>
+        </Row>
+        <hr className="whiteHr" />
+        <Row className="py-4">
+          <Col md={12} lg={4} className="homeImage1">
+            <Myimageslider width={"500px"} height={"400px"} />
+          </Col>
+          <Col
+            md={12}
+            lg={8}
+            className="d-flex flex-column justify-content-center align-items-center"
+          >
+            <div className="textTitleDiv2">
+              <h3>Unblur image with advanced AI</h3>
+              <p>
+                Retouch and fix the blurry image using our advanced AI
+                enhancement technology. Its time to unblur image for a clearer,
+                crisper, and more precise result.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <hr className="whiteHr" />
+        <Row className="py-4">
+          <Col
+            md={12}
+            lg={8}
+            className="d-flex flex-column justify-content-center align-items-center"
+          >
+            <div className="textTitleDiv2">
+              <h3>Unblur image with advanced AI</h3>
+              <p>
+                Retouch and fix the blurry image using our advanced AI
+                enhancement technology. Its time to unblur image for a clearer,
+                crisper, and more precise result.
+              </p>
+            </div>
+          </Col>
+          <Col md={12} lg={4} className="homeImage1">
+            <Myimageslider width={"500px"} height={"400px"} />
+          </Col>
+        </Row>
+        <hr className="whiteHr" />
       </Container>
     </div>
   );
--- fyp_frontend/src/components/Myimageslider/Myimageslider.jsx
+++ fyp_frontend/src/components/Myimageslider/Myimageslider.jsx
@@ -1,45 +1,47 @@
 import { ReactCompareSliderImage } from "react-compare-slider";
 import { ReactCompareSlider } from "react-compare-slider";
 import { ReactCompareSliderHandle } from "react-compare-slider";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./Myimageslider.css";
 import PropTypes from "prop-types";
 export default function Myimageslider(props) {
-    const [showSlider, setShowSlider] = useState(false);
-    useEffect(() => {
-        setShowSlider(true);
-      }, []);
+  const [showSlider, setShowSlider] = useState(false);
+  useEffect(() => {
+    setShowSlider(true);
+  }, []);
   return (
-    <ReactCompareSlider className={`homeSlider1 ${showSlider ? "show" : ""}`}
-              handle={
-                <ReactCompareSliderHandle
-                  buttonStyle={{ display: "none" }}
-                  linesStyle={{ width: 0 }}
-                />
-              }
-              changePositionOnHover={true}
-              boundsPadding={0}
-              itemOne={
-                <ReactCompareSliderImage
-                  alt="Image one"
-                  src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1784&q=70"
-                />
-              }
-              itemTwo={
-                <ReactCompareSliderImage
-                  alt="Image two"
-                  src="https://images.unsplash.com/photo-1437809781432-a957377661ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1784&q=70"
-                  style={{ transform: "scale(1.125)" }}
-                />
-              }
-              position={50}
-              style={{
-                height: props.height,
-                maxwidth: props.width,
-              }}
-            />
-  )
+    <ReactCompareSlider
+      className={`homeSlider1 ${showSlider ? "show" : ""}`}
+      handle={
+        <ReactCompareSliderHandle
+          buttonStyle={{ display: "none" }}
+          linesStyle={{ width: 0 }}
+        />
+      }
+      changePositionOnHover={true}
+      boundsPadding={0}
+      itemOne={
+        <ReactCompareSliderImage
+          alt="Image one"
+          src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1784&q=70"
+        />
+      }
+      itemTwo={
+        <ReactCompareSliderImage
+          alt="Image two"
+          src="https://images.unsplash.com/photo-1437809781432-a957377661ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1784&q=70"
+          style={{ transform: "scale(1.125)" }}
+        />
+      }
+      position={50}
+      style={{
+        height: props.height,
+        maxwidth: props.width,
+      }}
+    />
+  );
 }
 Myimageslider.propTypes = {
-    height: PropTypes.string.isRequired,
-    width: PropTypes.string.isRequired,
-  };
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+};
