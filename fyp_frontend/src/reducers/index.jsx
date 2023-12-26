5f7dffb469f596378e1f22ebeb3547f9f88eb6f7
Mayank Shetty <mayanks928@gmail.com>
2023-12-26 22:32:56 +0530

    requirements txt, redux initial setup , restarting project

---
+++ fyp_frontend/src/reducers/index.jsx
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import auth from './auth';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default combineReducers({
+    auth,
+});
