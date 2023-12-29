2b6e06c9c2b349641b01670f7582f4b0bef8e6a2
Mayank Shetty <mayanks928@gmail.com>
2023-12-29 20:48:47 +0530

    updated imports in urls

--- fyp_backend/accounts/urls.py
+++ fyp_backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView,GetCSRFToken,LoginView,LogoutView,CheckAuthenticatedView,DeleteAccountView,GetUsersView
+from .views import SignUpView,LoginView,LogoutView,DeleteAccountView,GetUsersView
 
 urlpatterns = [
     path('login',LoginView.as_view()),
